6814d9483d11f5c3176e2ff630955658dacd3cbb
aota18 <sapphire031794@gmail.com>
2021-03-24 17:39:18 +0900

    Boilerplate Setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import { App } from './components/app';
+import ReduxThunk from 'redux-thunk';
+import "./styles/styles.css";
+import { Provider } from 'react-redux';
+import reducers from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+/* Initialize Middleware */
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStoreWithMiddleware(reducers, composeWithDevTools())}>
+     <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+ 
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
